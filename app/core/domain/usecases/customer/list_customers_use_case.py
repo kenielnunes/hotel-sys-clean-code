e6537e7bbb212d8a7eb21a5e725f16342e307672
kenielnunes <kenielalves3@gmail.com>
2025-05-20 20:12:42 -0300

    refactor: list customer

--- app/core/domain/usecases/customer/list_customers_use_case.py
+++ app/core/domain/usecases/customer/list_customers_use_case.py
@@ -1,18 +1,10 @@
-from typing import List, Dict
-
+from typing import List
+from core.domain.entities.customer import Customer
 from core.infra.repositories.customer_repository import CustomerRepository
 
 class ListCustomersUseCase:
     def __init__(self, customer_repository: CustomerRepository):
         self._customer_repository = customer_repository
 
-    def execute(self) -> List[Dict]:
-        customers = self._customer_repository.find_all()
-        return [
-            {
-                "id": customer.id,
-                "name": customer.name,
-                "cpf": customer.cpf
-            }
-            for customer in customers
-        ] 
+    def execute(self) -> List[Customer]:
+        return self._customer_repository.find_all() 
