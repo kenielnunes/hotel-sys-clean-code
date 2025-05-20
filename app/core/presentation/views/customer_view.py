5749ae26f25233908ee1618a6e23e9c0301aa462
kenielnunes <kenielalves3@gmail.com>
2025-05-20 20:23:14 -0300

    fix: list customers

--- app/core/presentation/views/customer_view.py
+++ app/core/presentation/views/customer_view.py
@@ -3,8 +3,8 @@ from core.presentation.controllers.customer_controller import CustomerController
 from utils.cpf_validator import cpf_validator
 
 class CustomerView:
-    def __init__(self, controller: CustomerController):
-        self._controller = controller
+    def __init__(self, customer_controller: CustomerController):
+        self._customer_controller = customer_controller
 
     def get_valid_input(self, prompt: str, validator=None) -> str:
         while True:
@@ -18,7 +18,7 @@ class CustomerView:
             return value
 
     def create_customer(self):
-        print("\n=== Cadastro de Cliente ===")
+        print("\n=== Cadastrar Cliente ===")
         
         name = input("\nNome do cliente: ")
         if not name:
@@ -31,7 +31,21 @@ class CustomerView:
             return
 
         try:
-            customer = self._controller.create_customer(name=name, cpf=cpf)
+            customer = self._customer_controller.create_customer(name, cpf)
             print(f"\n\033[0;32mCliente cadastrado com sucesso! ID: {customer.id}\033[m")
         except ValueError as e:
-            print(f"\n\033[0;31mErro: {str(e)}\033[m") 
+            print(f"\n\033[0;31mErro ao cadastrar cliente: {str(e)}\033[m")
+
+    def list_customers(self):
+        print("\n=== Lista de Clientes ===")
+        customers = self._customer_controller.list_customers()
+        
+        if not customers:
+            print("\n\033[0;31mNenhum cliente cadastrado!\033[m")
+            return
+
+        for customer in customers:
+            print(f"\nID: {customer.id}")
+            print(f"Nome: {customer.name}")
+            print(f"CPF: {customer.cpf}")
+            print(f"Data de Cadastro: {customer.created_at.strftime('%d/%m/%Y %H:%M')}") 
