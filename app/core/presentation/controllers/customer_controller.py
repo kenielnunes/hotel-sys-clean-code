9aa56577b12cfe7a3c3f2efc0640bad8e74fa253
kenielnunes <kenielalves3@gmail.com>
2025-05-20 20:13:25 -0300

    refactor: methods list and create

--- app/core/presentation/controllers/customer_controller.py
+++ app/core/presentation/controllers/customer_controller.py
@@ -1,4 +1,5 @@
 from core.domain.usecases.customer.create_customer_use_case import CreateCustomerUseCase
+from core.domain.usecases.customer.list_customers_use_case import ListCustomersUseCase
 from core.infra.repositories.customer_repository import CustomerRepository
 from core.presentation.types.customer_types import (
     CustomerRequest,
@@ -8,13 +9,14 @@ from core.presentation.types.customer_types import (
 
 class CustomerController:
     def __init__(self):
-        self.customer_repository = CustomerRepository()
-        self.create_customer_use_case = CreateCustomerUseCase(self.customer_repository)
+        self._customer_repository = CustomerRepository()
+        self._create_customer_use_case = CreateCustomerUseCase(self._customer_repository)
+        self._list_customers_use_case = ListCustomersUseCase(self._customer_repository)
 
     def create(self, request: CustomerRequest) -> CustomerSingleResponse | CustomerErrorResponse:
         try:
             # Create customer
-            customer = self.create_customer_use_case.execute(
+            customer = self._create_customer_use_case.execute(
                 name=request['name'],
                 cpf=request['cpf']
             )
@@ -38,3 +40,17 @@ class CustomerController:
                 'status_code': 500,
                 'body': {'error': 'Internal server error'}
             }
+
+    def create_customer(self, name: str, cpf: str):
+        try:
+            customer = self._create_customer_use_case.execute(name, cpf)
+            return customer
+        except Exception as e:
+            raise
+
+    def list_customers(self):
+        try:
+            customers = self._list_customers_use_case.execute()
+            return customers
+        except Exception as e:
+            raise
