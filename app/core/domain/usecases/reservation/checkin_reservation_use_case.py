abff672d6457043d153573f96f166a4ab80b4f0f
kenielnunes <kenielalves3@gmail.com>
2025-05-20 20:11:23 -0300

    feat: checkout & checkin reservation

---
+++ app/core/domain/usecases/reservation/checkin_reservation_use_case.py
@@ -0,0 +1,21 @@
+from core.domain.entities.reservation import Reservation, ReservationStatus
+from core.infra.repositories.reservation_repository import ReservationRepository
+
+class CheckinReservationUseCase:
+    def __init__(self, reservation_repository: ReservationRepository):
+        self._reservation_repository = reservation_repository
+
+    def execute(self, reservation_id: int) -> Reservation:
+        reservation = self._reservation_repository.find_by_id(reservation_id)
+        if not reservation:
+            raise ValueError("Reserva não encontrada")
+
+        if reservation.status != ReservationStatus.RESERVED:
+            raise ValueError("Apenas reservas confirmadas podem fazer check-in")
+
+        reservation.status = ReservationStatus.CHECKED_IN
+        
+        print(f"reservation {reservation}")
+
+        # Salva a alteração
+        return self._reservation_repository.update(reservation) 
---
+++ app/core/domain/usecases/reservation/checkout_reservation_use_case.py
@@ -0,0 +1,18 @@
+from core.domain.entities.reservation import Reservation, ReservationStatus
+from core.infra.repositories.reservation_repository import ReservationRepository
+
+class CheckoutReservationUseCase:
+    def __init__(self, reservation_repository: ReservationRepository):
+        self._reservation_repository = reservation_repository
+
+    def execute(self, reservation_id: int) -> Reservation:
+        reservation = self._reservation_repository.find_by_id(reservation_id)
+        if not reservation:
+            raise ValueError("Reserva não encontrada")
+
+        if reservation.status != ReservationStatus.CHECKED_IN:
+            raise ValueError("Apenas reservas com check-in realizado podem fazer check-out")
+
+        reservation.status = ReservationStatus.CHECKED_OUT
+
+        return self._reservation_repository.update(reservation) 
